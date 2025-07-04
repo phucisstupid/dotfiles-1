a14718a4a7b8f5f1e1a7a9375ed3d09a8822f43d
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2025-07-04 22:12:22 +0700

    feat(install_sketchybar): better install sketchybar app font

--- install_sketchybar.sh
+++ install_sketchybar.sh
@@ -1,18 +1,18 @@
-echo "Installing Dependencies"
 # Packages
-brew install lua
-brew install switchaudio-osx
-brew install nowplaying-cli
-
+echo "Installing Dependencies"
+brew install lua switchaudio-osx nowplaying-cli
 brew tap FelixKratz/formulae
 brew install sketchybar
 
 # Fonts
-brew install --cask sf-symbols
-brew install --cask font-sf-mono
-brew install --cask font-sf-pro
+brew install --cask sf-symbols font-sketchybar-app-font font-sf-mono font-sf-pro
 
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+# Get latest icon_map.lua
+latest_tag=$(curl -s https://api.github.com/repos/kvndrsslr/sketchybar-app-font/releases/latest | grep '"tag_name":' | cut -d '"' -f 4)
+font_url="https://github.com/kvndrsslr/sketchybar-app-font/releases/download/${latest_tag}/icon_map.lua"
+output_path="$CONFIG_DIR/sketchybar/helpers/icon_map.lua"
+mkdir -p "$(dirname "$output_path")"
+curl -L "$font_url" -o "$output_path"
 
 # SbarLua
 (git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
@@ -22,4 +22,7 @@ git clone https://github.com/FelixKratz/dotfiles.git /tmp/dotfiles
 mv $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
 mv /tmp/dotfiles/.config/sketchybar $HOME/.config/sketchybar
 rm -rf /tmp/dotfiles
+
 brew services restart sketchybar
+sketchybar --reload
+
